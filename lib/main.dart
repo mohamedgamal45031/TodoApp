4ce2cd2aa0ef4dc9df159ebd8dd05010ce724fb9
GEMY <mohamedgamal45031@gmail.com>
2022-09-06 17:19:35 +0200

    todoAppFinal

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:udemy_flutter/shared/bloc_observer.dart';
+import 'package:udemy_flutter/shared/components/constants.dart';
+import 'package:udemy_flutter/shared/cubit/cubit.dart';
+import 'package:udemy_flutter/shared/cubit/states.dart';
+import 'package:udemy_flutter/shared/network/local/cache_helper.dart';
+import 'package:udemy_flutter/shared/network/remote/dio_helper.dart';
+import 'package:udemy_flutter/shared/styles/themes.dart';
+
+import 'layout/todo_app/todo_layout.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+  DioHelper.init();
+  await CacheHelper.init();
+  bool isDark = CacheHelper.getData(key: 'isDark');
+  token = CacheHelper.getData(key: 'token');
+
+
+  runApp(MyApp(
+    isDark: isDark,
+  ));
+}
+
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+  final bool isDark;
+
+  MyApp({
+    this.isDark,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (BuildContext context) => AppCubit()..createDatabase(),
+        child: BlocConsumer<AppCubit, AppStates>(
+        listener: (BuildContext context, AppStates state) {
+      if(state is AppInsertDatabaseState)
+      {
+        Navigator.pop(context);
+      }
+    },
+    builder: (BuildContext context, AppStates state) {
+    AppCubit cubit = AppCubit.get(context);
+
+    return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode:
+                AppCubit.get(context).isDark ? ThemeMode.dark : ThemeMode.light,
+            home: HomeLayout(),
+          );
+        },
+      ),
+    );
+  }
+}
